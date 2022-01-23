05219e7a7f54d4a4ad10a7af7311e1249af7ceeb
Stefan Jarina <stefan@jarina.io>
2022-01-23 19:55:22 +0100

    API: setup cors

--- backend/API/Startup.cs
+++ backend/API/Startup.cs
@@ -30,6 +30,7 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +49,7 @@ namespace API
             }
 
             app.UseRouting();
+            app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"); });
 
             app.UseAuthorization();
 

c1a07ee9748953535c3a3c31ed9308d56262ec28
Stefan Jarina <stefan@jarina.io>
2022-01-23 21:49:52 +0100

    SPA: add styling

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -1,36 +1,35 @@
-import { Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Container, createTheme, CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 import Catalog from '../../features/catalog/Catalog';
-import { Product } from '../models/product';
+import Header from './Header';
+import { useState } from 'react';
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data));
-  }, []);
+  const paletteType = darkMode ? 'dark' : 'light';
 
-  function addProduct() {
-    setProducts(prevState => [
-      ...prevState,
-      {
-        id: prevState.length + 101,
-        name: 'product' + (prevState.length + 1),
-        price: prevState.length * 100 + 100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200',
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212',
       },
-    ]);
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
   }
 
   return (
-    <>
-      <Typography variant="h1">ReStore</Typography>
-      <Catalog products={products} addProduct={addProduct} />
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
   );
 }
 

427807117dd42d02d51e344cbe8e7af2790f0998
Stefan Jarina <stefan@jarina.io>
2022-01-24 19:39:33 +0100

    API: add basket controller

--- backend/API/Startup.cs
+++ backend/API/Startup.cs
@@ -51,7 +51,10 @@ namespace API
             }
 
             app.UseRouting();
-            app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"); });
+            app.UseCors(opt =>
+            {
+                opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 

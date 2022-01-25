ab9ae6af650d1aee28504797fe5af3098e97817f
Stefan Jarina <stefan@jarina.io>
2022-01-25 20:00:30 +0100

    SPA: add filters fetching

--- frontend/src/features/catalog/Catalog.tsx
+++ frontend/src/features/catalog/Catalog.tsx
@@ -1,17 +1,27 @@
 import { useEffect } from 'react';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 import { useAppSelector, useAppDispatch } from '../../app/store/configureStore';
-import { fetchProductsAsync, productSelectors } from './catalogSlice';
+import {
+  fetchFilters,
+  fetchProductsAsync,
+  productSelectors,
+} from './catalogSlice';
 import ProductList from './ProductList';
 
 export default function Catalog() {
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status } = useAppSelector(state => state.catalog);
+  const { filtersLoaded, productsLoaded, status } = useAppSelector(
+    state => state.catalog
+  );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!productsLoaded) dispatch(fetchProductsAsync());
   }, [dispatch, productsLoaded]);
+
+  useEffect(() => {
+    if (!filtersLoaded) dispatch(fetchFilters());
+  }, [dispatch, filtersLoaded]);
 
   if (status.includes('pending'))
     return <LoadingComponent message="Loading products..." />;

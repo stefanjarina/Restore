caab502bcf654e831f4fa61f517b284cafb56217
Štefan Jarina <stefan@jarina.io>
2022-01-26 14:22:38 +0100

    SPA: convert login to use React Hook Forms

--- frontend/src/features/account/Login.tsx
+++ frontend/src/features/account/Login.tsx
@@ -8,23 +8,19 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import agent from '../../app/api/agent';
+import { FieldValues, useForm } from 'react-hook-form';
+import { LoadingButton } from '@mui/lab';
 
 export const Login = () => {
-  const [values, setValues] = useState({
-    username: '',
-    password: '',
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    agent.Account.login(values);
-  };
-
-  function handleInputChange(event: any) {
-    const { name, value } = event.target;
-    setValues({ ...values, [name]: value });
+  async function submitForm(data: FieldValues) {
+    await agent.Account.login(data);
   }
 
   return (
@@ -43,34 +39,35 @@ export const Login = () => {
       <Typography component="h1" variant="h5">
         Sign in
       </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(submitForm)}
+        noValidate
+        sx={{ mt: 1 }}>
         <TextField
           margin="normal"
           fullWidth
           label="Username"
-          name="username"
           autoComplete="username"
           autoFocus
-          onChange={handleInputChange}
-          value={values.username}
+          {...register('username')}
         />
         <TextField
           margin="normal"
           fullWidth
-          name="password"
           label="Password"
           type="password"
           autoComplete="current-password"
-          onChange={handleInputChange}
-          value={values.password}
+          {...register('password')}
         />
-        <Button
+        <LoadingButton
+          loading={isSubmitting}
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}>
           Sign In
-        </Button>
+        </LoadingButton>
         <Grid container>
           <Grid item>
             <Link to="/register">{"Don't have an account? Sign Up"}</Link>

6f3e11557a2e0438f4bf7c57163cfe5cce53c7b3
Štefan Jarina <stefan@jarina.io>
2022-01-26 12:28:06 +0100

    SPA: add login to agent

--- frontend/src/app/api/agent.ts
+++ frontend/src/app/api/agent.ts
@@ -83,7 +83,14 @@ const Basket = {
     requests.delete(`basket?productId=${productId}&quantity=${quantity}`),
 };
 
+const Account = {
+  login: (values: any) => requests.post('account/login', values),
+  register: (values: any) => requests.post('account/register', values),
+  currentUser: () => requests.get('account/currentUser'),
+};
+
 const agent = {
+  Account,
   Catalog,
   TestErrors,
   Basket,

2ecdfaf3f9dc36a91141b54ee1ce35cad7bcf822
Stefan Jarina <stefan@jarina.io>
2022-01-27 19:37:12 +0100

    API: map anonymous basket to user

--- backend/API/Controllers/AccountController.cs
+++ backend/API/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Threading.Tasks;
+using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -12,11 +15,13 @@ public class AccountController : BaseApiController
 {
     private readonly UserManager<User> _userManager;
     private readonly TokenService _tokenService;
+    private readonly StoreContext _context;
 
-    public AccountController(UserManager<User> userManager, TokenService tokenService)
+    public AccountController(UserManager<User> userManager, TokenService tokenService, StoreContext context)
     {
         _userManager = userManager;
         _tokenService = tokenService;
+        _context = context;
     }
 
     [HttpPost("login")]
@@ -26,10 +31,22 @@ public class AccountController : BaseApiController
         if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
             return Unauthorized();
 
+        var userBasket = await RetrieveBasket(loginDto.Username);
+        var anonBasket = await RetrieveBasket(Request.Cookies["buyerId"]);
+
+        if (anonBasket != null)
+        {
+            if (userBasket != null) _context.Baskets.Remove(userBasket);
+            anonBasket.BuyerId = user.UserName;
+            Response.Cookies.Delete("buyerId");
+            await _context.SaveChangesAsync();
+        }
+
         return new UserDto
         {
             Email = user.Email,
-            Token = await _tokenService.GenerateToken(user)
+            Token = await _tokenService.GenerateToken(user),
+            Basket = anonBasket != null ? anonBasket.MapBasketToDto() : userBasket.MapBasketToDto()
         };
     }
 
@@ -66,5 +83,19 @@ public class AccountController : BaseApiController
             Email = user.Email,
             Token = await _tokenService.GenerateToken(user)
         };
+    }
+
+    private async Task<Basket> RetrieveBasket(string buyerId)
+    {
+        if (string.IsNullOrEmpty(buyerId))
+        {
+            Response.Cookies.Delete("buyerId");
+            return null;
+        }
+
+        return await _context.Baskets
+            .Include(i => i.Items)
+            .ThenInclude(p => p.Product)
+            .FirstOrDefaultAsync(x => x.BuyerId == buyerId);
     }
 }
